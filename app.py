fef8ea270a99f3150e83bedcbcaec018962e8eec
Abhishek5467 <149815850+Abhishek5467@users.noreply.github.com>
2026-04-16 11:43:47 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,29 +1,10 @@
 import streamlit as st
-from streamlit.components.v1 import html
 
 st.set_page_config(layout="wide")
 
-st.title("Interactive Spline Editor")
+st.title("🎨 Interactive Spline Editor")
 
-html_code = """
-<!DOCTYPE html>
-<html>
-<head>
-  <style>
-    body { margin: 0; }
-    canvas { display: block; }
-  </style>
-</head>
-<body>
+with open("index.html", "r") as f:
+    html_code = f.read()
 
-<script src="https://unpkg.com/canvas-sketch@0.7.6/dist/canvas-sketch.umd.js"></script>
-
-<script>
-""" + open("script.js").read() + """
-</script>
-
-</body>
-</html>
-"""
-
-html(html_code, height=1100)
+st.html(html_code, height=1100)
