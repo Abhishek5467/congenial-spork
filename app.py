c9f271311d36a0b7c01213fab59ad0582bf9e0e5
Abhishek5467 <149815850+Abhishek5467@users.noreply.github.com>
2026-04-16 11:45:36 +0530

    updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,4 @@ st.set_page_config(layout="wide")
 
 st.title("🎨 Interactive Spline Editor")
 
-with open("index.html", "r") as f:
-    html_code = f.read()
-
-st.html(html_code, height=1100)
+st.iframe("index.html", height=1100)
